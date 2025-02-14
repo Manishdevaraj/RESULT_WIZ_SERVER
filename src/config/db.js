985a51edd5ff757106041c9987fb5decc570a76c
manishdevaraj <manishdevaraj03@gmail.com>
2025-02-14 07:58:18 +0530

    hj

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,13 @@
 import { DB_URL } from "../constants/env.js";
 import mongoose from "mongoose";
 
-  // Connect to MongoDB
-export const connectToDb=async()=>{
-  await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log('Connected to MongoDB');
-      
-    })
-    .catch((err) => {
-      console.error('Connection error', err);
-    });
-}
+  const MONGO_URI = "mongodb+srv://manish:manish%402005@resultwiz.oy14x.mongodb.net/resultwizDB?retryWrites=true&w=majority&appName=resultWIZ";
+
+export const connectToDb = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ Connection error", error);
+  }
+};
